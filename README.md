a7d26b57a0fc54f26c173f45c911e06b58447e07
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2026-01-04 13:48:20 +1100

    feat: init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# NotifyUserChanges
+
+Adds a notify option in the user context menu to get notified when a user changes voice channels or online status
+
+This is a reimplimentation of [BetterDiscord's FriendNotifications](https://github.com/mwittrien/BetterDiscordAddons/tree/c561c50fadf8ce7e7862e6a090e48196f6069b9b/Plugins/FriendNotifications) plugin and a fork of [NotifyUserStatus](https://github.com/D3SOX/Vencord/tree/plugin/notifyUserChanges) with updated compatibility for latest Vencord versions and userplugins folder support.
+
+![Context Menu](./context-menu.png)
+![Screenshot](./screenshot.png)
+
+## Installation
+
+You probably already know this, but just in case:
+
+1. Make sure you have [Vencord](https://github.com/Vendicated/Vencord) repository cloned and have the userplugins folder created in root/src directory.
+2. Clone this repository to your userplugins folder.
+3. run `pnpm install`, `pnpm build` and `pnpm inject` in the root of the Vencord repository.
---
+++ types.ts
@@ -0,0 +1,45 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export interface PresenceUpdate {
+    user: {
+        id: string;
+        username?: string;
+        global_name?: string;
+    };
+    clientStatus: {
+        desktop?: string;
+        web?: string;
+        mobile?: string;
+        console?: string;
+    };
+    guildId?: string;
+    status: string;
+    broadcast?: any; // what's this?
+    activities: Array<{
+        session_id: string;
+        created_at: number;
+        id: string;
+        name: string;
+        details?: string;
+        type: number;
+    }>;
+}
+
+export interface VoiceState {
+    userId: string;
+    channelId?: string;
+    oldChannelId?: string;
+    deaf: boolean;
+    mute: boolean;
+    selfDeaf: boolean;
+    selfMute: boolean;
+    selfStream: boolean;
+    selfVideo: boolean;
+    sessionId: string;
+    suppress: boolean;
+    requestToSpeakTimestamp: string | null;
+}
